3b8e1d7e2ce8854a3e7c2d8240468d4b94965ebd
Adrian Salceanu <adrian.salceanu@gmail.com>
2022-07-27 19:43:59 +0200

    assets for prod

--- routes.jl
+++ routes.jl
@@ -1,4 +1,4 @@
-# using Revise
+using Genie
 using Stipple
 using StippleUI
 using StipplePlotly
@@ -8,11 +8,11 @@ using Stipple.ModelStorage.Sessions
 
 using OscarStats.Oscars
 
+if Genie.Configuration.isprod()
+  Genie.Assets.assets_config!([Genie, Stipple, StippleUI, StipplePlotly], host = "https://cdn.statically.io/gh/GenieFramework")
+end
+
 Page("/", view = "views/hello.jl.html",
           layout = "layouts/app.jl.html",
           model = () -> init_from_storage(Oscar, debounce = 30) |> Oscars.handlers,
           context = @__MODULE__)
-
-route("err") do
-  throw("moo")
-end
